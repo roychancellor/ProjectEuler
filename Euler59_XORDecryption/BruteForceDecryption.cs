d9c22b24cd5f796081e701d17bd92728397709b4
Roy Chancellor <46630306+roychancellor@users.noreply.github.com>
2022-02-05 10:14:32 -0800

    Add files via upload

---
+++ Euler59_XORDecryption/BruteForceDecryption.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler59_XORDecryption
+{
+    public class BruteForceDecryption
+    {
+        public static void DecryptAndComputeSum()
+        {
+            List<int> asciiNumbers = new List<int>();
+
+            List<int> cipherNumbers = Utilities.GetCipherNumbers();
+
+            int iterationsCompleted = 0;
+            bool foundKey = false;
+            string thePlainText = string.Empty;
+
+            for (int i = Utilities.START_ASCII; i <= Utilities.END_ASCII && !foundKey; i++)
+            {
+                for (int j = Utilities.START_ASCII; j <= Utilities.END_ASCII && !foundKey; j++)
+                {
+                    for (int k = Utilities.START_ASCII; k <= Utilities.END_ASCII && !foundKey; k++)
+                    {
+                        iterationsCompleted++;
+                        if (iterationsCompleted % 1000 == 0)
+                        {
+                            Console.WriteLine($"Iterations completed: {iterationsCompleted}");
+                        }
+                        
+                        foundKey = Utilities.MakePlainTextFromCipherText(new List<int>{i, j, k}, cipherNumbers, out string plainText);
+                        
+                        if (foundKey)
+                        {
+                            Console.WriteLine($"Key: {(char)i}{(char)j}{(char)k} => {plainText}");
+                            thePlainText = plainText;
+                        }
+                    }
+                }
+            }
+
+            // Compute the sum of the ASCII values of the plain text
+            int sum = Utilities.ComputeSumOfPlainTextAscii(thePlainText);
+            Console.WriteLine($"\nThe sum is: {sum}");
+        }
+    }
+}
---
+++ Euler60_PrimePairSets/Euler60_PrimePairSets/ClassRunner.cs
@@ -0,0 +1,12 @@
+﻿namespace Euler60_PrimePairSets
+{
+    public class ClassRunner
+    {
+        public static void Main(string[] args)
+        {
+            //string[] argsToTest = new[] { "--s", "3", "--n", "1000000", "--t", "5", "--i", "3000" };
+            string[] argsToTest = new[] { "--help" };
+            PrimePairSets.Run(argsToTest);
+        }
+    }
+}
